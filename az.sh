dae169b0807ae4ebeda75262571b71decfefcfc4
lakshma009 <lakshma1119@gmail.com>
2019-11-13 12:49:33 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,16 +1,19 @@
-az login -u yrajavarapu@nisum.com -p Nisum.1090@e
+#az login -u yrajavarapu@nisum.com -p Nisum.1090@e
 
-az group create --name myResource1 --location eastus
+#az group create --name myResource1 --location eastus
 
-az vm create \
-   --resource-group myResource1 \
-   --name myVM1 \
-   --image CentOS \
-   --admin-username azureuser \
-   --admin-password Azure.123456@e
+#az vm create \
+#   --resource-group myResource1 \
+ #  --name myVM1 \
+  # --image CentOS \
+  # --admin-username azureuser \
+  # --admin-password Azure.123456@e
    
 #az vm open-port --port 80 --resource-group myResource1 --name myVM1
 
-az vm open-port --port 22 --resource-group myResource1 --name myVM1
+#az vm open-port --port 22 --resource-group myResource1 --name myVM1
 
-sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@40.121.48.14
+sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@40.121.48.14 << EOF
+ls -lrt
+EOF
+
