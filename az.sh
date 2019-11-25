e867465930916f2f1c106c52a02310494d310346
lakshma009 <lakshma1119@gmail.com>
2019-11-25 14:48:38 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,23 +1,23 @@
-#az login -u yrajavarapu@nisum.com -p Nisum.1090@e
+az login -u yrajavarapu@nisum.com -p Nisum.1090@e
 
-#az group create --name myResource1 --location eastus
+az group create --name myResource1 --location eastus
 
-#az vm create \
-#   --resource-group myResource1 \
- #  --name myVM1 \
-  # --image CentOS \
-  # --admin-username azureuser \
-  # --admin-password Azure.123456@e
+az vm create \
+   --resource-group myResource1 \
+   --name myVM1 \
+   --image CentOS \
+   --admin-username azureuser \
+   --admin-password Azure.123456@e
    
-#az vm open-port --port 80 --resource-group myResource1 --name myVM1
+az vm open-port --port 80 --resource-group myResource1 --name myVM1
 
-#az vm open-port --port 22 --resource-group myResource1 --name myVM1
+az vm open-port --port 22 --resource-group myResource1 --name myVM1
 
 sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@40.121.48.14 << 'ENDSSH'
 sudo -S <<< "Azure.123456@e" yum update -y
 sudo -S <<< "Azure.123456@e" yum install java-1.8.0-openjdk -y
-#sudo -S <<< "Azure.123456@e" wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz
-#sudo -S <<< "Azure.123456@e" tar -xvzf apache-tomcat-9.0.27.tar.gz
+sudo -S <<< "Azure.123456@e" wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz
+sudo -S <<< "Azure.123456@e" tar -xvzf apache-tomcat-9.0.27.tar.gz
 #scp -r /var/root/.jenkins/workspace/Azure/hello-world-war-1.0.0.war azureuser@40.121.48.14:/home/azureuser/apache-tomcat-9.0.27/webapps 
 #sudo -S <<< “Azure.123456@e” scp -r /var/root/.jenkins/workspace/Azure/hello-world-war-1.0.0.war azureuser@40.121.48.14:/home/azureuser/apache-tomcat-9.0.27/webapps
 #scp -r /Users/nisum/Desktop/hello-world-war-1.0.0.war azureuser@40.121.48.14:/home/azureuser/apache-tomcat-9.0.27/webapps
